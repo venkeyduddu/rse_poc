620e38e6dc27a119e35ba72f32de036e6562f385
venkatesh <venkatesh@brio.co.in>
2022-04-29 21:39:27 +0530

    test2

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+application = Flask(__name__)
 
-   return "Hello World"
+@application.route("/")
+def home():
+    return "Hello, World!!!"
 
-if __name__ == '__main__':
-   app.run()
+if __name__ == "__main__":
+    application.run()
